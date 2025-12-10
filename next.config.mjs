697532fc3ed2abb652cd9aa8b9d55949463576ce
Oreoluwa-Smartsafe <oreoluwa.ibikunle@smartsafeuk.com>
2025-12-10 15:15:04 +0100

    fix version

--- next.config.mjs
+++ next.config.mjs
@@ -5,53 +5,60 @@ import withTM from 'next-transpile-modules';
 
 const withTranspiledModules = withTM(['lodash-es']);
 
-
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = dirname(__filename);
+
 const nextConfig = {
-  reactStrictMode: false,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  webpack: (config, { isServer }) => {
-    // Fix for the issue with ESM packages
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        module: false,
-      };
+    reactStrictMode: false,
+
+    // 👇 Force Webpack (fix for Next.js 16)
+    experimental: {
+        turbo: false,
+    },
+
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+                module: false,
+            };
+        }
+        return config;
+    },
+
+    transpilePackages: [
+        "@ant-design",
+        "rc-util",
+        "rc-tree",
+        "rc-pagination",
+        "rc-picker",
+        "geist",
+        "rc-table",
+        "string-width",
+        "antd",
+        "rc-notification",
+        "rc-tooltip",
+        'lodash-es',
+        '@ant-design/plots',
+        "@rc-component/util"
+    ],
+
+    images: {
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'storage.googleapis.com',
+            port: '',
+            pathname: '/**',
+        }]
     }
-    return config;
-  },
-  transpilePackages: [
-    "@ant-design", 
-    "rc-util",
-    "rc-tree", 
-    "rc-pagination",
-    "rc-picker",
-    "geist",
-    "rc-table",
-    "string-width",
-    "antd",
-    "rc-notification", 
-    "rc-tooltip",
-    'lodash-es', 
-    '@ant-design/plots',
-    "@rc-component/util"
-  ],
-  images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
-    ]
-  }
 };
 
 export default withTranspiledModules(nextConfig);
